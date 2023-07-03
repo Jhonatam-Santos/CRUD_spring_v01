f4d50eeb7e2cc12953da7d831c9d678d3f943c96
Jhonatam-Santos <jhonatamns@outlook.com>
2023-07-02 23:43:09 -0300

    Primeiro commit iniciando a recaptulação sobre jdbc e as rotas padrão de add, get, update e delete.

---
+++ src/main/java/com/example/crud_v01/CrudV01Application.java
@@ -0,0 +1,13 @@
+package com.example.crud_v01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudV01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudV01Application.class, args);
+    }
+
+}
